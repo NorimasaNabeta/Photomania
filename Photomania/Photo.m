2e608ac88351b4c67f9a7b4a738d89f7b466797f
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-08-09 22:46:43 +0900

    lecture's demo

---
+++ Photomania/Photo.m
@@ -0,0 +1,21 @@
+//
+//  Photo.m
+//  Photomania
+//
+//  Created by Norimasa Nabeta on 2012/08/09.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import "Photo.h"
+#import "Photographer.h"
+
+
+@implementation Photo
+
+@dynamic imageURL;
+@dynamic subtitle;
+@dynamic title;
+@dynamic unique;
+@dynamic whoTook;
+
+@end
---
+++ Photomania/Photographer.m
@@ -0,0 +1,18 @@
+//
+//  Photographer.m
+//  Photomania
+//
+//  Created by Norimasa Nabeta on 2012/08/09.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import "Photographer.h"
+#import "Photo.h"
+
+
+@implementation Photographer
+
+@dynamic name;
+@dynamic photos;
+
+@end
